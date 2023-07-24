0482fb8885bfc71fd112dd85fdbbaf15ebb9b77b
Totoroxdxd <assignmentsforelaine@gmail.com>
2023-07-24 23:34:35 +0800

    fix global class scrip bug

--- scenes/start_room/zombie.gd
+++ scenes/start_room/zombie.gd
@@ -1,10 +1,22 @@
 extends CharacterBody2D
 #variables for movement
 @export var speed = 60
+
+#Code from sample_level
+const pushback: float = 100
+const pushback_fric: float = 0.08
+#Vector2.ZERO -> .ZERO implies that the vector has no length? no mangnitude??
+#Technically can just wrtie Vector2 coordinates at the back, but people on godot say put .zero so that math will be more consistant?? but why?
+var pushback_vel := Vector2.ZERO 
+
+var player_hurt = false
+var player_attack = false
 var player_chase = false
 var player = null
+
+@onready var animation = $AnimationPlayer
 #variables for combat
-var health = 100
+# var health = 100
 
 
 func _physics_process(delta):
@@ -14,10 +26,16 @@ func _physics_process(delta):
 		direction = direction.normalized()
 		velocity = direction * speed
 
-		$AnimatedSprite2D.play("walking")
-		
+		animation.play("walk")
+	elif player_hurt:
+		animation.play("attack")
+		pushback_vel = lerp(pushback_vel, Vector2.ZERO, pushback_fric)
+		velocity = pushback_vel
+		move_and_slide()
+	elif player_attack:
+		pass
 	else:
-		$AnimatedSprite2D.play("idle")
+		$AnimationPlayer.play("idle")
 	move_and_slide()
 
 func _on_detection_area_body_entered(body):
@@ -29,14 +47,14 @@ func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
 	
-func enemy():
-	pass
-
-
 
 func _on_zombie_hitbox_body_entered(body):
-	pass # Replace with function body.
+	pass
+
+func _on_hurtbox_received_damage(eff_dmg, dir):
+	player_hurt = true
+	
 
 
-func _on_zombie_hitbox_body_exited(body):
-	pass # Replace with function body.
+func _on_health_manager_died():
+	queue_free()
